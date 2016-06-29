4b55b0a0d153a1457c9288200e973bfb13dd4f8f
theodore.r <theodore.r@zohocorp.com>
2016-06-29 16:22:14 +0530

    Suite updated

--- TestMultiplex/src/com/movieapp/test/AllTests.java
+++ TestMultiplex/src/com/movieapp/test/AllTests.java
@@ -5,7 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({CategoryResourceTest.class, CustomerResourceTest.class, ExtraResourceTest.class,
+	MovieResourceTest.class, MovieShowResourceTest.class, ScreenResourceTest.class, 
+	SeatResourceTest.class, ShowResourceTest.class, ShowSeatResourceTest.class, 
+	TicketResourceTest.class})
 public class AllTests {
 
 }
